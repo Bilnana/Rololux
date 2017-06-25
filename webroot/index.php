339fd0ead6eb0c35f5c5084a33fcd6f0895904c5
Biljana <biljanalj85@gmsil.com>
2017-06-26 00:33:31 +0200

    Initial state with all dependences.

---
+++ webroot/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html lang="sr-RS-latin">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rololux</title>
+
+    <meta name="Biljana Ljubojevic" content="Task">
+</head>
+
+<h1>Biljana:)</h1>
+<body>
+
+</body>
+</html>
