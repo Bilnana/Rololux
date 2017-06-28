e1a9b9d4c23cece692709df270dbb8dfa8afe3cb
marKoZz94 <marko.94ns@live.com>
2017-06-28 14:43:49 +0200

    Added contact section with icons and map.

--- webroot/index.php
+++ webroot/index.php
@@ -131,6 +131,32 @@
             </div>
         </article>
     </section>
+
+    <!-- SECTION CONTACT -->
+<section id="rololux1">
+    <div class="roletne">
+        <header class="roletne-head1">
+            <h1>Kontakt</h1>
+        </header>
+        <div class="contact-map row row-content">
+            <section class="col-sm-12 col-md-6 col-lg-6">
+                <adress class="contact-details">
+                    <h2 class="phone">063/ 8-063-531 | 064/ 11-59-532</h2>
+                    <h2 class="fax">021/ 2466-467</h2>
+                    <h2 class="mail">rololuxns@yahoo.com | miklos@drupal.rs</h2>
+                    <h2 class="adress">Subotička 67 21 000 Novi Sad, Srbija</h2>
+                </adress>
+            </section>
+            <div class="slider1 col-sm-12 col-md-6 col-lg-6">
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2809.404352493143!2d19.834526515422457!3d45.239615556422315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475b101758e3b51d%3A0xec137a793663a8be!2sNarodnog+fronta+23%2C+Novi+Sad+21000!5e0!3m2!1sen!2srs!4v1465553186255" allowfullscreen="" frameborder="0" height="450" width="100%"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+    <!-- SECTION CONTACT -->
+
     <footer class="footer">
         <div class="row">
             <div class="col-sm-3">
