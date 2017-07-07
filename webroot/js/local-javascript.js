ceed88c6d74491b2c30268226b430d7c5eb025d6
Biljana <biljanalj85@gmsil.com>
2017-07-07 09:50:46 +0200

    Delete carausel functions.

--- webroot/js/local-javascript.js
+++ webroot/js/local-javascript.js
@@ -1,27 +1,4 @@
 
-    /****************** SLIDER HEADER *****************/
-    $(function() {
-        $('#crausel1').carousel({
-        interval: 2000
-        })
-    });
-
-      $(function() {
-        $('#crausel-big').carousel({
-        interval: 2000
-        })
-    });
-
-
-
-       $(function() {
-        $('#crausel2').carousel({
-        interval: 2000
-        })
-    });
-
-    /***************** SLIDER HEADER *****************/
-
     /***************** SCROLL EFECT ******************/
 
     $('a[href^="#"]').on('click', function(event) {
